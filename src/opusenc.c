36f86ec2aca817ed8991e74d04e7884613058601
Tristan Matthews <tmatth@videolan.org>
2023-04-19 09:37:30 -0400

    opusenc: add support for higher frame sizes
    These were added in opus 1.2. while not particularly useful for writing Ogg Opus files,
    they are included for completeness and/or debugging.
    
    Signed-off-by: Mark Harris <mark.hsj@gmail.com>

--- src/opusenc.c
+++ src/opusenc.c
@@ -158,7 +158,11 @@ static void usage(void)
   printf(" -S, --speech       Tune low bitrates for speech (override automatic detection)\n");
   printf(" --comp n           Set encoding complexity (0-10, default: 10 (slowest))\n");
   printf(" -F, --framesize n  Set maximum frame size in milliseconds\n");
+#ifdef OPUS_FRAMESIZE_120_MS
   printf("                      (2.5, 5, 10, 20, 40, 60, 80, 100, 120, default: 20)\n");
+#else
+  printf("                      (2.5, 5, 10, 20, 40, 60, 80, 100, 120, default: 20)\n");
+#endif
   printf(" --expect-loss n    Set expected packet loss in percent (default: 0)\n");
   printf(" --downmix-mono     Downmix to mono\n");
   printf(" --downmix-stereo   Downmix to stereo (if >2 channels)\n");
@@ -747,12 +751,18 @@ int main(int argc, char **argv)
           else if (strcmp(optarg,"20")==0) opus_frame_param=OPUS_FRAMESIZE_20_MS;
           else if (strcmp(optarg,"40")==0) opus_frame_param=OPUS_FRAMESIZE_40_MS;
           else if (strcmp(optarg,"60")==0) opus_frame_param=OPUS_FRAMESIZE_60_MS;
+#ifdef OPUS_FRAMESIZE_120_MS
           else if (strcmp(optarg,"80")==0) opus_frame_param=OPUS_FRAMESIZE_80_MS;
           else if (strcmp(optarg,"100")==0) opus_frame_param=OPUS_FRAMESIZE_100_MS;
           else if (strcmp(optarg,"120")==0) opus_frame_param=OPUS_FRAMESIZE_120_MS;
+#endif
           else {
             fatal("Invalid framesize: %s\n"
-              "Value is in milliseconds and must be 2.5, 5, 10, 20, 40, 60, 80, 100, or 120.\n",
+#ifdef OPUS_FRAMESIZE_120_MS
+              "Value is in milliseconds and must be 2.5, 5, 10, 20, 40, 60, 80, 100 or 120.\n",
+#else
+              "Value is in milliseconds and must be 2.5, 5, 10, 20, 40, or 60.\n",
+#endif
               optarg);
           }
           frame_size = opus_frame_param <= OPUS_FRAMESIZE_40_MS
