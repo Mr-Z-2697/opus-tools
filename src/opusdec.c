70af1d3ee194faa5ba909eae14c7453b249a3a59
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-10-29 15:20:02 +0800

    opusdec: ???

--- src/opusdec.c
+++ src/opusdec.c
@@ -117,8 +117,8 @@
 #define MAXI(_a,_b)      ((_a)>(_b)?(_a):(_b))
 #define CLAMPI(_a,_b,_c) (MAXI(_a,MINI(_b,_c)))
 
-/* 120ms at 48000 */
-#define MAX_FRAME_SIZE (960*6)
+/* 120ms at 96000 */
+#define MAX_FRAME_SIZE (960*6*2)
 
 #ifdef HAVE_LIBSNDIO
 struct sio_hdl *hdl;
@@ -1151,17 +1151,17 @@ int main(int argc, char **argv)
         as described in the OggOpus spec.  But for commandline tools
         like opusdec it can be desirable to exactly preserve the original
         sampling rate and duration, so we have a resampler here.*/
-      // if (rate!=48000 && resampler==NULL)
-      // {
-      //    int err;
-      //    resampler = speex_resampler_init(channels, 48000, rate, 5, &err);
-      //    if (err!=0)
-      //    {
-      //       fprintf(stderr, "resampler error: %s\n",
-      //        speex_resampler_strerror(err));
-      //    }
-      //    speex_resampler_skip_zeros(resampler);
-      // }
+      if (rate!=48000 && resampler==NULL)
+      {
+         int err;
+         resampler = speex_resampler_init(channels, 48000, rate, 5, &err);
+         if (err!=0)
+         {
+            fprintf(stderr, "resampler error: %s\n",
+             speex_resampler_strerror(err));
+         }
+         speex_resampler_skip_zeros(resampler);
+      }
       outsamp=audio_write(permuted_output?permuted_output:output, channels,
        nb_read, fout, resampler, clipmem, dither?&shapemem:0, file_output,
        rate, link_read, link_out, fp);
